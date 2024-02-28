2cb1bd49fdb1b8a014018ad5bec554a0760c6153
jones58 <108466184+jones58@users.noreply.github.com>
2024-02-28 22:04:29 +0000

    edit sidebar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,15 +1,39 @@
+import React, { useState } from "react";
+
 function Sidebar() {
+  const [open, setOpen] = useState(false);
+
+  const toggle = () => {
+    setOpen(!open); // Toggle the state of open
+  };
+
   return (
-    <div className="w-1/3 bg-black h-screen">
-      <div className="text-white text-2xl p-10 flex flex-col justify-between space-y-10">
-        <a className="text-2xl">Menu</a>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/portfolio">Portfolio</a>
-        <a href="/process">Process</a>
-        <a href="/contact">Contact</a>
-      </div>
-    </div>
+    <>
+      {open ? (
+        /* if open, display the sidebar 1/3 */
+        <div className="w-[30vw] bg-black h-screen">
+          <div className="text-white text-2xl p-10 flex flex-col justify-between space-y-10">
+            <a className="text-2xl">Menu</a>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/portfolio">Portfolio</a>
+            <a href="/process">Process</a>
+            <a href="/contact">Contact</a>
+            <h1 className="p-10" onClick={toggle}>
+              X
+            </h1>
+          </div>
+        </div>
+      ) : (
+        /* if not open, display the sidebar 1/6 */
+        <div className="w-[10vw] bg-black h-screen">
+          <div className="text-white text-2xl p-10 flex flex-col justify-between space-y-10">
+            <a className="text-2xl">Menu</a>
+            <h1 onClick={toggle}>X</h1>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
